ce8714fa041554511332d08b7450d5d8efb23508
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-09-24 15:26:43 -0700

    Initial Commit

---
+++ Car-Pool Savings Calclator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Car-Pool Savings Calclator
+//
+//  Created by Amanpreeet Sandhu on 2024-09-24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
